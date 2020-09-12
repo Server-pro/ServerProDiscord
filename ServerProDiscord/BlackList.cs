66863f8af9a135b9a4a33fd727d5c95878c341e3
CatSandwich <59626141+CatSandwich@users.noreply.github.com>
2020-09-12 15:04:03 -0400

    Added blacklist

---
+++ ServerProDiscord/BlackList.cs
@@ -0,0 +1,34 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerProDiscord
+{
+    static class BlackList
+    {
+        public static readonly string BlackListPath = "../../../../blacklist.txt";
+        public static List<string> _blackList;
+        public static async Task DeleteBlackList(SocketMessage msg)
+        {
+            foreach (string s in _blackList)
+            {
+                if (msg.Content.ToLower().Contains(s))
+                {
+                    await msg.DeleteAsync();
+                    await WarnBlackList(msg);
+                    return;
+                }
+            }
+        }
+
+        public static async Task WarnBlackList(SocketMessage msg)
+        {
+            await msg.Channel.SendMessageAsync($"{msg.Author.Mention} has been warned!");
+        }
+
+
+    }
+
+}
--- ServerProDiscord/Program.cs
+++ ServerProDiscord/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
@@ -15,24 +17,25 @@ namespace ServerProDiscord
         {
             _client = new DiscordSocketClient();
             _client.Log += Log;
+            _client.MessageReceived += BlackList.DeleteBlackList;
 
-            string token = System.IO.File.ReadAllText("../../../../token.token");
-
-            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-            // var token = File.ReadAllText("token.txt");
-            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+            string token = System.IO.File.ReadAllText(TokenPath);
+            BlackList._blackList = System.IO.File.ReadLines(BlackList.BlackListPath).ToList();
 
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
+            
 
-            // Block this task until the program is closed.
             await Task.Delay(-1);
         }
         private Task Log(LogMessage msg)
         {
-            Console.WriteLine($"Logging: {msg.ToString()}");
+            Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
         }
+
+
+
+        static readonly string TokenPath = "../../../../.token";
     }
 }
