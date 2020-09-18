3bf5b1934a9067daeb5e0ee5fca6aff6d22fd841
CatSandwich <59626141+CatSandwich@users.noreply.github.com>
2020-09-18 14:18:32 -0400

    Token was in last push - reuploading with token reading from file. Token was regenerated.

--- ServerProDiscord/Config.cs
+++ ServerProDiscord/Config.cs
@@ -12,13 +12,13 @@ namespace ServerProDiscord
     {
         private static Map _map;
 
-        public static string Token = "NzU0ODcyODc3NzY1MDk5NjAw.X17EJg.WnNF6SGuUNMU0uR4vVJaQaBCe2Y";
+        public static string Token;
 
         public static bool DevEnv;
         public static RuntimeProfile Profile;
         public static ulong[] Admins;
 
-        public static void Init(string configPath)
+        public static void Init(string configPath = "../../../../config.yml", string tokenPath = "../../../../token.txt")
         {
             var deserializer = new Deserializer();
             var configRaw = System.IO.File.ReadAllText(configPath);
@@ -28,6 +28,8 @@ namespace ServerProDiscord
             DevEnv = _map.DevEnv;
             Profile = DevEnv ? _map.DevProfile : _map.ProdProfile;
             Admins = _map.Admins;
+
+            Token = System.IO.File.ReadAllText(tokenPath);
         }
 
 
